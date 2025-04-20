b04c71b215e1c762ef7cf2832631ba67750266be
obilovincent1234 <obilochigozie5@gmail.com>
2025-04-20 14:59:50 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,38 @@
 import os
+import threading
 import logging
 from pyrogram.raw.all import layer
 from pyrogram import Client, idle, __version__
-
+from flask import Flask
 from config import Config
 
+# Set up logging
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
-
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+# Start Flask in a separate thread
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Koyeb (Bot is running)"
+
+def run_flask():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
+
+threading.Thread(target=run_flask).start()
+
+# Start the bot
 if not os.path.isdir(Config.DOWNLOAD_LOCATION):
     os.makedirs(Config.DOWNLOAD_LOCATION)
 
-if not Config.BOT_TOKEN:
-    logger.error("Please set BOT_TOKEN in config.py or as env var")
+if not Config.BOT_TOKEN or not Config.API_ID or not Config.API_HASH:
+    logger.error("Missing config values")
     quit(1)
-
-if not Config.API_ID:
-    logger.error("Please set API_ID in config.py or as env var")
-    quit(1)
-
-if not Config.API_HASH:
-    logger.error("Please set API_HASH in config.py or as env var")
-    quit(1)
-
 
 bot = Client(
     "All-Url-Uploader",
@@ -38,9 +44,8 @@ bot = Client(
 )
 
 bot.start()
-logger.info("Bot has started.")
-logger.info("**Bot Started**\n\n**Pyrogram Version:** %s \n**Layer:** %s", __version__, layer)
-logger.info("Developed by github.com/kalanakt Sponsored by www.netronk.com")
+logger.info("**Bot Started**")
+logger.info("**Pyrogram Version:** %s \n**Layer:** %s", __version__, layer)
 idle()
 bot.stop()
-logger.info("Bot Stopped ;)")
+logger.info("**Bot Stopped**")
